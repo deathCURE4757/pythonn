0baf6ee729121420ae4859601c98bdf50606c796
Sahil Singh <sahilsingh050245@gmail.com>
2025-04-13 06:54:51 -0400

    both new:capital and cart menu

---
+++ conCapital.py
@@ -0,0 +1,20 @@
+capital ={
+    'USA': 'Washington D.C.',
+    'Canada': 'Ottawa',
+    'Mexico': 'Mexico City',
+    'Brazil': 'Brasilia',
+    'UK': 'London',
+}
+#print(dir(capital))
+#print(help(capital))
+#(capitals.get('USA'))
+#print(capital.get('USA'))
+#capital.update({'Germany': 'Berlin'})
+#print(capital)
+#capital.pop('USA')
+#capital.popitem()
+
+#keys = capital.keys()
+#print(keys)
+#values = capital.values()
+#print(values)
---
+++ menuBill.py
@@ -0,0 +1,40 @@
+menu = {"pizza": 10,
+     "burger": 5,
+     "soda": 2, 
+     "salad": 4, 
+     "fries": 3,
+     "dessert": 6,
+     "water": 1,
+     "coffee": 2,
+     "tea": 2,
+     "juice": 3,
+     "sandwich": 5,
+     "pasta": 8,
+     "steak": 15,
+     "chicken": 12 }
+cart = []
+total = 0
+
+print("Welcome to the restaurant menu!")
+print("-------Here is the menu--------")
+# Display the menu
+for key, value in menu.items():
+    print(f"{key:10}: ${value:.2f}")    
+print("-------End of the menu--------")
+print("Please select items from the menu to add to your cart.")
+print("Type 'done' when you are finished.")
+while True:
+    item = input("Enter an item from menu: ").lower()
+    if item == "done":
+        break
+    elif item in menu:
+        cart.append(item)
+        total += menu[item]
+        print(f"{item} added to cart.")
+    else:
+        print("Item not found in the menu.")
+print("-------Your cart--------")
+for item in cart:
+    print(item)
+print(f"Total: ${total:.2f}")
+# Display the total amount
