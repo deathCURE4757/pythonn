70c22f97ec085191c90001a422fe6453f1bf6deb
Sahil Singh <sahilsingh050245@gmail.com>
2025-04-13 06:32:10 -0400

    quiz 18 per pro

--- quizgame.py
+++ quizgame.py
@@ -18,6 +18,33 @@ score = 0
 question_num = 0
 for question in question:
     print("------------------------")
-    print("question")
-    
-   
+    print(question)
+    for option in options[question_num]:
+        print(option)
+        
+    guess = input("Enter (A, B, C, D): ").upper()
+    guesses.append(guess)    
+    if guess == answer[question_num]:
+        print("Correct!")
+        score += 1
+    else:
+        print("Incorrect! The correct answer is", answer[question_num])
+  
+    question_num += 1
+
+print("------------------------")
+print("Results")
+print("------------------------")
+print("Answers: ", end="")
+for answer in answer:
+    print(answer, end=" ")
+print()    
+
+print("Guesses: ", end="")
+for guess in guesses:
+    print(guess, end=" ")
+
+score = int(float(score) / float(len(question)) * 100)  # Ensuring floating-point division
+print("Score: ", score, "%")
+
+
